369836ce5da8882ed8b262856b1e440368c95d09
jai-d3v <jayaprakash.j@optisolbusiness.com>
2022-12-26 11:49:11 +0530

    Implemented Seeders

--- src/__seeders__/seed_users.ts
+++ src/__seeders__/seed_users.ts
@@ -1,5 +1,5 @@
-import { AppDataSource } from './data-source';
-import { User } from './entity/User';
+import { AppDataSource } from '../data-source';
+import { User } from '../entity/User';
 import bcryptjs from 'bcryptjs';
 
 AppDataSource.initialize()
